a7f88cc133d06a775d8d199f2e6d67d2e26d0dd0
Avery Couture <avery.couture@login8.ufhpc>
2025-07-15 20:18:13 -0400

    Start of PICL Test directory.  Compiles main.f with ppiclF library and has MPI functioning with main.f & job file.

---
+++ libpicl/PICL_Test/job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=PICLtest
+#SBATCH --output=run.log
+#SBATCH --error=run.err
+#SBATCH --mail-type=none
+#SBATCH --ntasks=8
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=8gb                     # Per node
+#SBATCH --time=00-00:02:00             # Walltime in hh:mm:ss or d-hh:mm:ss
+#SBATCH --account=bala1s
+#SBATCH --qos=bala1s
+##SBATCH --contiguous
+##SBATCH --exclusive
+date;hostname;pwd
+module purge 
+
+module load gcc/14.2.0
+module load openmpi/5.0.7
+
+EXEC=PICL_Test
+
+ntasks=$SLURM_NTASKS
+nodes=$SLURM_JOB_NUM_NODES
+
+echo "Executable =" $EXEC
+echo "Current Directory =" `pwd`
+
+echo Using `which mpirun`
+srun -N $(($nodes)) -n $(($ntasks)) --mpi=${HPC_PMIX} $EXEC -v 2
