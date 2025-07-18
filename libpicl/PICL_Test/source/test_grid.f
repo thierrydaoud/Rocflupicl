c691a19e19980abab0993aba8731ba41e4a274c1
Avery Couture <avery.couture@login9.ufhpc>
2025-07-17 20:34:50 -0400

    Added test grid generator

--- libpicl/PICL_Test/source/test_grid.f
+++ libpicl/PICL_Test/source/test_grid.f
@@ -1,12 +1,47 @@
-      SUBROUTINE test_grid_CreateGrid(x,y,z,dx,dy,dz)
+      SUBROUTINE test_CreateGrid(gIn,NCells,ProcID,NProc,gOut)
       
       IMPLICIT NONE
-      ! x(1:2): overall grid 1:min,2:max length in x,y,&z
-      ! dx, dy, dz: cell lengths 
 
-      REAL :: x(2),y(2),z(2),dx,dy,dz
+      ! Input/Output
+      ! gridIn(1:2,1:3) - fluid domain 1:min,2:max faces in x,y,&z
+      ! Cells - per processor
+      ! Num Proc - number of processors
+      ! gridOut 1:3 - centroids, 4:6 - cell lengths, 7 - cell volume
+      INTEGER*4 NCells, NProc, ProcID
+      REAL*8    gIn(2,3),gOut(7,NCells)
 
-      PRINT*, 'Create Grid called successfully!'
+      ! Local
+      INTEGER*4 i, j, k, cellCount, NCellsPerDim
+      REAL*8    delta(3), xMin(3), dx(3)
+
+      NCellsPerDim = INT(NCells**(1.0/3.0))
+      ! Create uniform rectangular grid
+      DO i = 1,3
+        !processors fluid domain length
+        delta(i) = (gIn(2,1) - gIn(1,1))/NProc
+        xMin(i)  = delta(i)*NProc ! This processors Min x,y,z
+        dx(i)    = delta(i)/NCellsPerDim
+      END DO
+
+      cellCount = 0
+      DO i = 1,NCellsPerDim 
+        DO j = 1,NCellsPerDim
+          DO k = 1,NCellsPerDim
+            cellCount = cellCount + 1
+            gOut(1,cellCount) = xMin(1) + (i+0.5-1)*dx(1) !x centroid
+            gOut(2,cellCount) = xMin(2) + (j+0.5-1)*dx(2) !y centroid
+            gOut(3,cellCount) = xMin(3) + (k+0.5-1)*dx(3) !z centroid
+            gOut(4,cellCount) = dx(1)
+            gOut(5,cellCount) = dx(2)
+            gOut(6,cellCount) = dx(3)
+            gOut(7,cellCount) = dx(1)*dx(2)*dx(3)
+          END DO !k
+        END DO !j
+      END DO !i
+
+      IF(cellCount .NE. NCells) THEN
+        PRINT*,'ERROR, wrong NCells generated on proc', NProc
+      END IF
 
       RETURN
       END SUBROUTINE
--- libpicl/PICL_Test/source/test_main.f
+++ libpicl/PICL_Test/source/test_main.f
@@ -5,38 +5,55 @@
       INCLUDE 'mpif.h'
       INCLUDE 'PPICLF'
 
-      INTEGER*4 test, np, nid, icomm, ierr
-      REAL*8 dia_p, x(2), y(2), z(2), dx, dy, dz
+      INTEGER*4 i, j, k, nproc, nid, icomm, ierr
 
+      ! Grid variables
+      INTEGER*4 nCells
+      REAL*8    grid(7,100), gridDomain(2,3), xMin(3),xMax(3)
+
+      ! Particle variables
+      !REAL*8 
 
       CALL MPI_Init(ierr)
       icomm = MPI_COMM_WORLD
-      CALL MPI_Comm_size(icomm,np,ierr)
+      CALL MPI_Comm_size(icomm,nproc,ierr)
       CALL MPI_Comm_rank(icomm,nid,ierr)  
 
       IF(nid .EQ. 0) THEN
         PRINT*, ''
         PRINT*, '*****************************************************'
         PRINT*, 'ppiclF test run starting'
-        PRINT*, 'Number of Processors:',np
+        PRINT*, 'Number of Processors:',nproc
         PRINT*, ''
-       END IF
-      CALL ppiclf_comm_InitMPI(icomm, nid, np)
-
-      IF(nid .EQ. 0) THEN
-        dia_p = 0.1
-        dx = 1.0
-        dy = 1.0
-        dz = 1.0
-        x(1) = 0.0
-        x(2) = 1.0
-        y(1) = 0.0
-        y(2) = 1.0
-        z(1) = 0.0
-        z(2) = 1.0 
-        CALL test_particles_CreateParticles(dia_p)
-        CALL test_grid_CreateGrid(x,y,z,dx,dy,dz)
       END IF
+
+      ! This creates a uniform rectangular grid
+      DO i = 1,3 !3D uniform grid
+        gridDomain(1,i) = 0.0D0 !Min
+        gridDomain(2,i) = 10.0D0 !Max
+      END DO
+      nCells = 1000 !Per processor & must be equal to grid() Num columns
+      CALL test_CreateGrid(gridDomain,nCells,nid,nproc,grid)
+ 
+      ! START grid test
+      DO i = 1,3
+        xMin(i) = 1.0D9
+        xMax(i) = 1.0D-9
+      END DO
+      DO j = 1,nCells
+        DO i = 1,3
+          IF(grid(i,j) .LT. xMin(i)) xMin(i) = grid(i,j)
+          IF(grid(i,j) .GT. xMax(i)) xMax(i) = grid(i,j)
+        END DO
+      END DO     
+      CALL MPI_BARRIER(icomm,ierr)
+      DO i = 1,3
+        PRINT*, 'Proc, Dim, xMin, xMax:', nid, i, xMin(i), xMax(i)
+      END DO 
+      ! END grid test
+
+      CALL ppiclf_comm_InitMPI(icomm, nid, nproc)
+      ! Create particle array
 
       IF(nid .EQ. 0) THEN
         PRINT*, ''
