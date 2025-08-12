d61a5978f80386c9b559df9cdd4953782532ee6a
Avery Couture <avery.couture@login8.ufhpc>
2025-08-11 21:26:08 -0400

    Updating test code

--- libpicl/PICL_Test/testCode/picl_test.f
+++ libpicl/PICL_Test/testCode/picl_test.f
@@ -10,7 +10,7 @@
       INCLUDE 'mpif.h'
       INCLUDE 'PPICLF'
 
-      INTEGER*4 i, j, l, nproc, nid, icomm, ierr
+      INTEGER*4 i, j, l, nproc, nid, icomm, ierr, test
       REAL*8    nndistTemp, nndist, PI, k
 
       ! Grid variables
@@ -22,13 +22,16 @@
       ! Particle variables
       REAL*8    part_y(PPICLF_LRS,PPICLF_LPART), pdia, C2Pratio, 
      >          part_r(PPICLF_LRP,PPICLF_LPART),T_truth(PPICLF_LPART),
-     >          totErr, numErr, xp, yp, zp,r_npl,r_npt
+     >          totErr, numErr, xp, yp, zp,r_npl,r_npt, i_err
       INTEGER*4 npart_local,totalParticles, ip
 
       ! Projection variables
       REAL*8    wsum, dSQl, dSQi, dist, CellVol, GaussianConst,
      >          w(PPICLF_LEE),part_feedbk(PPICLF_LPART),
      >          TrueFeedback(PPICLF_LEE), error
+
+      CHARACTER*50 filename, testcase, procString
+
 
       PI = 4.0D0*ATAN(1.0) ! pi
       k  = 3.0D0 ! wave number
@@ -38,11 +41,9 @@
       CALL MPI_Init(ierr)
       icomm = MPI_COMM_WORLD
       CALL MPI_Comm_size(icomm,nproc,ierr)
-      CALL MPI_Comm_rank(icomm,nid,ierr) 
-      CALL ppiclf_comm_InitMPI(icomm, nid, nproc)
-      CALL test_PrintBanner(1,nid,nproc)
-      CALL MPI_BARRIER(icomm,ierr)
-
+      CALL MPI_Comm_rank(icomm,nid,ierr)
+      WRITE(procString, '(I0)') nid
+      IF(nid .LT. 11) procString = '0' // procString
 ! Grid Setup
 !**********************************************************************
       ! Create rectangular grid
@@ -60,26 +61,6 @@
 
       CALL test_CreateGrid(gridDomain,nCells,nid,nproc,grid,proc_ncells)
       CALL MPI_BARRIER(icomm, ierr)
-
-! ppiclF Inputs
-!**********************************************************************
-      ! Fluid Domain Min/Max
-      x_per_min = gridDomain(1,1)
-      x_per_max = gridDomain(2,1)
-      y_per_min = gridDomain(1,2)
-      y_per_max = gridDomain(2,2)
-      z_per_min = gridDomain(1,3)
-      z_per_max = gridDomain(2,3)
-   
-      ! Periodicity Setup
-      x_per_flag     = 1
-      y_per_flag     = 1
-      z_per_flag     = 1
-      ang_per_flag   = 0
-      ang_per_angle  = 0.0D0
-      ang_per_xangle = 0.0D0
-      ang_per_rin    = 0.0D0
-      ang_per_rout   = 0.0D0
 
       ! Find cell filter search distance
       filterTemp   = 1.0D-9 !dummy
@@ -114,11 +95,20 @@
         CALL MPI_Allreduce(dx_min(i),dx_min(i),1,MPI_DOUBLE,
      >                                      MPI_MIN,iComm,ierr)
       END DO
+
+      ! Fluid Domain Min/Max
+      x_per_min = gridDomain(1,1)
+      x_per_max = gridDomain(2,1)
+      y_per_min = gridDomain(1,2)
+      y_per_max = gridDomain(2,2)
+      z_per_min = gridDomain(1,3)
+      z_per_max = gridDomain(2,3)
+
       CALL MPI_BARRIER(icomm,ierr)
 
 ! Particle Setup   
 !********************************************************************** 
-      C2Pratio = 3.0 ! C2Pratio = cell dx / particle dx
+      C2Pratio = 3.0 ! C2Pratio = cell dx to particle dx ratio
       pdia     = MIN(dx_min(1)/C2Pratio,
      >               dx_min(2)/C2Pratio,
      >               dx_min(3)/C2Pratio)
@@ -129,7 +119,9 @@
       CALL MPI_Allreduce(r_npl,r_npt,1,MPI_DOUBLE,
      >                                      MPI_SUM,iComm,ierr)
       totalParticles = INT(r_npt)
+
       CALL MPI_BARRIER(icomm,ierr)
+
       IF(nid .EQ. 0) PRINT*,'Total Particles:',totalParticles 
       part_r = 0.0D0
       rhop   = 7730.0D0 ! steel particles
@@ -147,150 +139,191 @@
      >                              *(0.5D0*pdia)**3 ! particle volume
         part_r(PPICLF_R_JSPL,i) = 1.0D0 ! Super Particle Loading 
       END DO
+
       nndistTemp  = 4.0D0*pdia
       CALL MPI_Allreduce(nndistTemp,nndist,1,MPI_DOUBLE,
      >                                      MPI_MAX,iComm,ierr)
+
       CALL MPI_BARRIER(icomm,ierr)
+! ppiclF Inputs and test case setup
+!**********************************************************************
+
+      DO test = 1,9
+
+        ! Periodicity flag Setup
+        CALL test_setperiodic(x_per_flag,y_per_flag,z_per_flag,
+     >                                            test,testcase)
+        ! Will handle angular periodicity separately
+        ang_per_flag   = 0
+        ang_per_angle  = 0.0D0
+        ang_per_xangle = 0.0D0
+        ang_per_rin    = 0.0D0
+        ang_per_rout   = 0.0D0  
+
+        IF(test .EQ. 1) THEN
+          CALL ppiclf_comm_InitMPI(icomm, nid, nproc)
+          CALL test_PrintBanner(1,nid,nproc)
+          CALL MPI_BARRIER(icomm,ierr)
+        END IF
   
-     
 ! Start ppiclF Calls
 !**********************************************************************
-      PPICLF_TEST = .TRUE.
-      PPICLF_PERTEST = .TRUE.
-      CALL ppiclf_solve_InitParticle(2,3,0,npart_local,
-     >                               part_y,part_r,filter,nndist)
-      PPICLF_TEST = .TRUE.
-      PPICLF_PERTEST = .TRUE.
-      CALL ppiclf_solve_Initialize(x_per_flag, x_per_min, x_per_max,
-     >                             y_per_flag, y_per_min, y_per_max, 
-     >                             z_per_flag, z_per_min, z_per_max, 
-     >                             ang_per_flag, ang_per_angle, 
-     >                             ang_per_xangle, ang_per_rin,
-     >                                                  ang_per_rout)
-      PPICLF_TEST = .TRUE.
-      PPICLF_PERTEST = .TRUE.
-      CALL ppiclf_comm_InitOverlapMesh(proc_ncells,grid)
-      CALL ppiclf_solve_InterpFieldUser(PPICLF_R_JT,tpF)
-      CALL ppiclf_solve_InitSolve
-      CALL MPI_BARRIER(icomm,ierr)
-      DO ie = 1,proc_ncells
-        CALL ppiclf_solve_GetProFld(ie,1,feedback(ie))
-      END DO
-      CALL MPI_BARRIER(icomm,ierr)
+        PPICLF_TEST    = .TRUE.
+        PPICLF_PERTEST = .TRUE.
+        PPICLF_OVERLAP = .FALSE.
+        CALL ppiclf_solve_InitParticle(2,3,0,npart_local,
+     >                                 part_y,part_r,filter,nndist)
+        PPICLF_TEST = .TRUE.
+        PPICLF_PERTEST = .TRUE.
+        CALL ppiclf_solve_Initialize(x_per_flag, x_per_min, x_per_max,
+     >                               y_per_flag, y_per_min, y_per_max, 
+     >                               z_per_flag, z_per_min, z_per_max, 
+     >                               ang_per_flag, ang_per_angle, 
+     >                               ang_per_xangle, ang_per_rin,
+     >                                                    ang_per_rout)
+        PPICLF_TEST = .TRUE.
+        PPICLF_PERTEST = .TRUE.
+        CALL ppiclf_comm_InitOverlapMesh(proc_ncells,grid)
+        CALL ppiclf_solve_InterpFieldUser(PPICLF_R_JT,tpF)
+        CALL ppiclf_solve_InitSolve
+        CALL MPI_BARRIER(icomm,ierr)
+        DO ie = 1,proc_ncells
+          CALL ppiclf_solve_GetProFld(ie,1,feedback(ie))
+        END DO
+        CALL MPI_BARRIER(icomm,ierr)
 
 ! Print Interpolation results 
 !**********************************************************************
  
-      IF(ppiclf_npart .GT. 0) THEN
-        DO i = 1,ppiclf_npart
-          xp = ppiclf_y(PPICLF_JX,i)
-          yp = ppiclf_y(PPICLF_JY,i)
-          zp = ppiclf_y(PPICLF_JZ,i) 
-          T_truth(i) = 1.0D0 
-     >       + 1.0D0*SIN((k/(2*PI))*
-     >       (xp/(gridDomain(2,1)-gridDomain(1,1))))
-     >       + 1.0D0*COS((k/(2*PI))*
-     >       (yp/(gridDomain(2,2)-gridDomain(1,2))))
-     >       + 1.0D0*SIN((k/(2*PI))*
-     >       (zp/(gridDomain(2,3)-gridDomain(1,3))))
-        END DO
-        totErr = 0.0D0
-        numErr = 0.0D0
-        DO i = 1,ppiclf_npart
-          totErr = totErr + ABS(ppiclf_rprop(PPICLF_R_JT,i)-T_truth(i))
-     >            /T_truth(i)*100.0D0
-          numErr = numErr + 1.0D0
-        END DO
-      ELSE
-        totErr = 0.0D0
-        numErr = 0.0D0
-      END IF
-      CALL MPI_BARRIER(icomm,ierr)
-      CALL MPI_Allreduce(totErr,totErr,1,MPI_DOUBLE,
-     >                                      MPI_SUM,iComm,ierr)
-      CALL MPI_Allreduce(numErr,numErr,1,MPI_DOUBLE,
-     >                                      MPI_SUM,iComm,ierr)
-      CALL MPI_BARRIER(icomm,ierr)
-      ! Print Bin Data
-      IF(nid .EQ. 0) THEN
-        PRINT*,'Average Interpolation Error for all particles:'
-     >         ,totErr/numErr,'%'
-      END IF
-      CALL MPI_BARRIER(icomm,ierr)
+        IF(ppiclf_npart .GT. 0) THEN
+          filename = ''
+          filename = TRIM(testcase) // '_' // 'Interpolation Proc_'
+     >                  // TRIM(procString) // '.txt'
+          OPEN(UNIT=300,FILE=TRIM(filename), STATUS='REPLACE',
+     >                                      ACTION='WRITE')
+          WRITE(300,*) 'Particle ID, x, y,',
+     >                        'z, Interpolation Error (%).'
+          DO i = 1,ppiclf_npart
+            xp = ppiclf_y(PPICLF_JX,i)
+            yp = ppiclf_y(PPICLF_JY,i)
+            zp = ppiclf_y(PPICLF_JZ,i) 
+            T_truth(i) = 1.0D0 
+     >         + 1.0D0*SIN((k/(2*PI))*
+     >         (xp/(gridDomain(2,1)-gridDomain(1,1))))
+     >         + 1.0D0*COS((k/(2*PI))*
+     >         (yp/(gridDomain(2,2)-gridDomain(1,2))))
+     >         + 1.0D0*SIN((k/(2*PI))*
+     >         (zp/(gridDomain(2,3)-gridDomain(1,3))))
+          END DO
+          totErr = 0.0D0
+          numErr = 0.0D0
+          DO i = 1,ppiclf_npart
+            i_err = ABS(ppiclf_rprop(PPICLF_R_JT,i)-T_truth(i))
+     >                  /T_truth(i)*100.0D0
+            totErr = totErr + i_err 
+            numErr = numErr + 1.0D0
+            WRITE(300,*) i, xp, yp, zp, i_err
+          END DO
+          CLOSE(UNIT=300)
+        ELSE
+          totErr = 0.0D0
+          numErr = 0.0D0
+        END IF
+        CALL MPI_BARRIER(icomm,ierr)
+        CALL MPI_Allreduce(totErr,totErr,1,MPI_DOUBLE,
+     >                                        MPI_SUM,iComm,ierr)
+        CALL MPI_Allreduce(numErr,numErr,1,MPI_DOUBLE,
+     >                                        MPI_SUM,iComm,ierr)
+        CALL MPI_BARRIER(icomm,ierr)
+        ! Print Bin Data
+        IF(nid .EQ. 0) THEN
+          PRINT*,'Average Interpolation Error for all particles:'
+     >           ,totErr/numErr,'%'
+        END IF
+        CALL MPI_BARRIER(icomm,ierr)
 
 ! Print Projection Results
 !**********************************************************************
-      ! Find true projection result
-      IF(nproc .EQ. 1) THEN
-        TrueFeedback = 0.0D0
-        DO ip = 1,npart_local
-          w     = 0.0D0
-          wsum  = 0.0D0
-          ! Loop to find individual cell weightings
+        ! Find true projection result
+        IF(nproc .EQ. 1) THEN
+          TrueFeedback = 0.0D0
+          DO ip = 1,npart_local
+            w     = 0.0D0
+            wsum  = 0.0D0
+            ! Loop to find individual cell weightings
+            DO ie = 1,proc_ncells
+              dSQi = 0.0D0
+              dSQl = 0.0D0
+              DO l = 1,3
+                IF(ppiclf_linperiodic(l) .AND. 
+     >                               ppiclf_EqualDomain(l)) THEN
+                  dSQl = MIN( (grid(l,ie) - part_y(l,ip))**2, 
+     >             ( (gridDomain(2,l) - gridDomain(1,l)) -
+     >                   ABS(grid(l,ie) - part_y(l,ip)) )**2 )
+                ELSE
+                  dSQl = (grid(l,ie) - part_y(l,ip))**2
+                END IF
+                dSQi = dSQi + dSQl
+              END DO !l
+
+              dist = SQRT(dSQi)
+              CellVol = grid(7,ie)
+              GaussianConst = 2.305D0
+              w(ie) = ABS(CellVol*EXP(-GaussianConst*(dist**2)
+     >                  / (CellVol**(2.0D0/3.0D0))))
+              wsum = wsum + w(ie)
+            END DO !ie
+
+            ! Will range from 0 to 3
+            part_feedbk(ip) = part_y(1,ip)
+     >              /(gridDomain(2,1) - gridDomain(1,1)) +       
+     >              part_y(2,ip)
+     >              /(gridDomain(2,2) - gridDomain(1,2)) +       
+     >              part_y(3,ip)
+     >              /(gridDomain(2,3) - gridDomain(1,3))        
+            DO ie = 1,proc_ncells
+              TrueFeedback(ie) = TrueFeedback(ie) + 
+     >                           w(ie)/wsum*part_feedbk(ip)
+            END DO !ie
+          END DO !ip
+          PRINT*, '**********************************************'
+          PRINT*, 'Lets see how many cells 1 particle projects to'
+          PRINT*, 'Cell feedback weight loop!'
           DO ie = 1,proc_ncells
-            dSQi = 0.0D0
-            dSQl = 0.0D0
-            DO l = 1,3
-              IF(ppiclf_linperiodic(l) .AND. ppiclf_EqualDomain(l)) THEN
-                dSQl = MIN( (grid(l,ie) - part_y(l,ip))**2, 
-     >           ( (gridDomain(2,l) - gridDomain(1,l)) -
-     >                 ABS(grid(l,ie) - part_y(l,ip)) )**2 )
-              ELSE
-                dSQl = (grid(l,ie) - part_y(l,ip))**2
-              END IF
-              dSQi = dSQi + dSQl
-            END DO !l
+            IF(w(ie)/wsum .gt. 0.001) PRINT*,'cell feedback weight >',
+     >                                            '0.1%',ie,w(ie)/wsum
+          END DO
 
-            dist = SQRT(dSQi)
-            CellVol = grid(7,ie)
-            GaussianConst = 2.305D0
-            w(ie) = ABS(CellVol*EXP(-GaussianConst*(dist**2)
-     >                / (CellVol**(2.0D0/3.0D0))))
-            wsum = wsum + w(ie)
-          END DO !ie
-
-          ! Will range from 0 to 3
-          part_feedbk(ip) = part_y(1,ip)
-     >            /(gridDomain(2,1) - gridDomain(1,1)) +       
-     >            part_y(2,ip)
-     >            /(gridDomain(2,2) - gridDomain(1,2)) +       
-     >            part_y(3,ip)
-     >            /(gridDomain(2,3) - gridDomain(1,3))        
+          filename = TRIM(testcase) // '_' //'FeedbackSolution.txt'
+          OPEN(UNIT=499,FILE=filename, STATUS='REPLACE',ACTION='WRITE')
+          WRITE(499,*) 'Feedback true solution'
+          WRITE(499,*) 'Cell ID, x_centroid, y_centroid,',
+     >                        'z_centroid, feedback., Percent error'
           DO ie = 1,proc_ncells
-            TrueFeedback(ie) = TrueFeedback(ie) + 
-     >                         w(ie)/wsum*part_feedbk(ip)
-          END DO !ie
-        END DO !ip
-        PRINT*, '**********************************************'
-        PRINT*, 'Lets see how many cells 1 particle projects to'
-        PRINT*, 'Cell feedback weight loop!'
-        DO ie = 1,proc_ncells
-          IF(w(ie)/wsum .gt. 0.001) PRINT*,'cell feedback weight > 0.1%'
-     >                                ,ie,w(ie)/wsum
-        END DO
+            error = ABS(feedback(ie) - TrueFeedback(ie))
+     >              / TrueFeedback(ie) * 100.0
+            WRITE(499,*) ie, grid(1,ie), grid(2,ie),
+     >                           grid(3,ie),TrueFeedback(ie) ,error
+          END DO
+          CLOSE(UNIT=499)
+        END IF
 
-        WRITE(499,*) 'Feedback true solution'
-        WRITE(499+nid,*) 'Cell ID, x_centroid, y_centroid,
-     >                      z_centroid, feedback., Percent error'
-        DO ie = 1,proc_ncells
-          error = ABS(feedback(ie) - TrueFeedback(ie))
-     >            / TrueFeedback(ie) * 100.0
-          WRITE(499,*) ie, grid(1,ie), grid(2,ie),
-     >                         grid(3,ie),TrueFeedback(ie) ,error
-        END DO
-      END IF
 
-      CALL MPI_BARRIER(icomm,ierr)
-
-      IF(proc_ncells .GT. 0) THEN
-          WRITE(401+nid,*) 'Cell ID, x_centroid, y_centroid,
-     >                      z_centroid, feedback.'
-        DO ie = 1,proc_ncells
-          WRITE(401+nid,*) ie, grid(1,ie), grid(2,ie),
-     >                         grid(3,ie),feedback(ie)
-        END DO
-      END IF
-
+        filename = ''
+        CALL MPI_BARRIER(icomm,ierr)
+        IF(proc_ncells .GT. 0) THEN
+          filename = TRIM(testcase) // '_' // 'Feedback      Proc_' //
+     >                              TRIM(procString) // '.txt'
+          OPEN(UNIT=400,FILE=filename, STATUS='REPLACE',ACTION='WRITE')
+          WRITE(400,*) 'Cell ID, x_centroid, y_centroid,',
+     >                        'z_centroid, feedback.'
+          DO ie = 1,proc_ncells
+            WRITE(400,*) ie, grid(1,ie), grid(2,ie),
+     >                           grid(3,ie),feedback(ie)
+          END DO
+          CLOSE(UNIT=400)
+        END IF
+      END DO !test
 ! Test CreateBin variations
 !********************************************************************** 
       IF(nproc .EQ. 1) THEN
@@ -314,18 +347,21 @@
 !          WRITE(100+j,*) 'y domain:', ppiclf_xdrange(2,2)
 !          WRITE(100+j,*) 'z domain:', ppiclf_xdrange(2,3)
 !
-          WRITE(100,*) 'Number of Processors, x bins, y bins, z bins,
-     >                  , total bins, Percent of Processors In Use:' 
+          filename = 'CreateBinResults.txt'
+          OPEN(UNIT=100,FILE=filename, STATUS='REPLACE',ACTION='WRITE')
+          WRITE(100,*) 'Number of Processors, x bins, y bins, z bins,',
+     >                  ', total bins, Percent of Processors In Use:' 
           DO i = 1,512
             ppiclf_np = i
             num_bins = ppiclf_n_bins(1)*
      >                 ppiclf_n_bins(2)*ppiclf_n_bins(3)
             CALL ppiclf_comm_CreateBin
-            WRITE(100+j,*) ppiclf_np, 
+            WRITE(100,*) ppiclf_np, 
      >         ppiclf_n_bins(1), ppiclf_n_bins(2), ppiclf_n_bins(3),
      >         num_bins , num_bins/ppiclf_np*100
-!          END DO
-        END DO
+          END DO
+          CLOSE(UNIT=100)
+!        END DO
       END IF
 
 
@@ -384,9 +420,10 @@
       REAL*8    gIn(2,3), gOut(7,PPICLF_LEE)
 
       ! Local
-      INTEGER*4 i, j, k, ii, nx_per_proc
-      REAL*8    dx(3)
-      LOGICAL   GOOD
+      INTEGER*4    i, j, k, ii, nx_per_proc
+      REAL*8       dx(3)
+      CHARACTER*50 filename
+      LOGICAL      FIRST, GOOD
 
       ! Creates rectangular grid
       DO i = 1,3
@@ -398,6 +435,7 @@
       nx_per_proc = CEILING(REAL(NCells(1))/REAL(NProc))
       IF(NProc .EQ. 1) nx_per_proc = NCells(1)
       procCells = 0
+      FIRST = .TRUE.
       DO i = (nx_per_proc*ProcID)+1, nx_per_proc*(ProcID+1)
         DO j = 1,NCells(2)
           DO k = 1,NCells(3)
@@ -418,12 +456,20 @@
               END IF
             END DO
             IF(GOOD) THEN
-              WRITE(200+ProcID,*) procCells, gOut(1,procCells),
+              IF(FIRST) THEN
+                WRITE(filename,'(A,I0,A)') 'GridList              ',
+     >         ' Proc_0', ProcID, '.txt'
+                OPEN(UNIT=200,FILE=filename, STATUS='REPLACE',
+     >            ACTION='WRITE')
+              END IF
+              FIRST = .FALSE.
+              WRITE(200,*) procCells, gOut(1,procCells),
      >                        gOut(2,procCells),gOut(3,procCells)
             END IF
           END DO !k
         END DO !j
       END DO !i
+      CLOSE(UNIT=200)
 
       RETURN
       END SUBROUTINE
@@ -483,3 +529,54 @@
       RETURN
       END SUBROUTINE
 !----------------------------------------------------------------------
+      SUBROUTINE test_setperiodic(xf,yf,zf,i,tc)
+
+      ! Input/Output
+      ! xf - x periodic flag
+      ! yf - y periodic flag
+      ! zf - z periodic flag
+      ! i  - test iteration
+      ! tc - test case name
+      IMPLICIT NONE
+
+      INTEGER*4 xf, yf, zf, i
+      CHARACTER*50 tc
+
+      xf = 0
+      yf = 0
+      zf = 0
+
+      IF(i .EQ. 1) THEN
+        tc = 'NonPeriodic' 
+      ELSE IF(i .EQ. 2) THEN
+        tc = 'Periodic_x' 
+        xf = 1
+      ELSE IF(i .EQ. 3) THEN
+        tc = 'Periodic_y' 
+        yf = 1
+      ELSE IF(i .EQ. 4) THEN
+        tc = 'Periodic_z' 
+        zf = 1
+      ELSE IF(i .EQ. 5) THEN
+        tc = 'Periodic_xy' 
+        xf = 1
+        yf = 1
+      ELSE IF(i .EQ. 6) THEN
+        tc = 'Periodic_xz' 
+        xf = 1
+        zf = 1
+      ELSE IF(i .EQ. 7) THEN
+        tc = 'Periodic_yz'
+        yf = 1
+        zf = 1 
+      ELSE IF(i .EQ. 8) THEN
+        tc = 'Periodic_xyz' 
+        xf = 1
+        yf = 1
+        zf = 1           
+      END IF
+
+      RETURN
+      END SUBROUTINE
+!----------------------------------------------------------------------
+ 
